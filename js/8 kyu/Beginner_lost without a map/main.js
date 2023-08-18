d9e805c11e5cef14b9539e9d6059db675fbabd12
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-08-18 08:30:42 -0700

    MAPPPPPSSSSS

---
+++ js/8 kyu/Beginner_lost without a map/main.js
@@ -0,0 +1,13 @@
+/*
+Description:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+//Solution:
+function maps(x) {
+  return x.map((el) => el * 2);
+}
