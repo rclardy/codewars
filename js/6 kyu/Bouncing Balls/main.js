f6330a7a6aae8686f93daefd5407f4b00f160c8e
CodingClardy <28507502+CodingClardy@users.noreply.github.com>
2023-08-29 09:24:27 -0700

    bouncing balls simplier

--- js/6 kyu/Bouncing Balls/main.js
+++ js/6 kyu/Bouncing Balls/main.js
@@ -42,3 +42,17 @@ function bouncingBall(h, bounce, window) {
   }
   return falls;
 }
+
+//Simpler solution
+function bouncingBall(h, bounce, window) {
+  if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1;
+  let seen = 0;
+
+  while (h > window) {
+    seen += 1;
+    h *= bounce;
+    if (h > window) seen += 1;
+  }
+
+  return seen;
+}
