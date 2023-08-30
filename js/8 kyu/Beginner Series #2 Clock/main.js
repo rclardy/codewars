896ff242161e0e6b5e766335c8ae60e600d43330
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2023-08-30 07:32:07 -0700

    Counting Milliseconds

---
+++ js/8 kyu/Beginner Series #2 Clock/main.js
@@ -0,0 +1,29 @@
+/*
+Description:
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+Example:
+
+h = 0
+m = 1
+s = 1
+
+result = 61000
+
+Input constraints:
+
+    0 <= h <= 23
+    0 <= m <= 59
+    0 <= s <= 59
+
+*/
+
+//Solution
+function past(h, m, s) {
+  const sec = 1000;
+  const min = 60000;
+  const hour = 3600000;
+  return h * hour + m * min + s * sec;
+}
