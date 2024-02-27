f74769cea3e6250ef32e2e0edcd98406d4e64587
CoffeeNomad <28507502+rclardy@users.noreply.github.com>
2024-02-26 20:56:44 -0800

    I need to pratice on my array methods...

--- js/8 kyu/Sort and Star/main.js
+++ js/8 kyu/Sort and Star/main.js
@@ -9,22 +9,6 @@ You should not remove or add elements from/to the array.
 
 //Solution:
 
-function twoSort(arr) {
-  const alphabet = {};
-  for (let i = 97; i <= 122; i++) {
-    //ASCII codes for lowercase letters
-    alphabet[String.fromCharCode(i)];
-  }
-  arr.sort();
-
-  const firstString = arr[0];
-
-  let result = '';
-  for (let i = 0; i < firstString.length; i++) {
-    if (i !== 0) {
-      result += '***';
-    }
-    result += firstString[i];
-  }
-  return result;
+function twoSort(s) {
+  return s.sort()[0].split('').join('***');
 }
